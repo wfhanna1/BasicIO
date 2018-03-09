c1636964bda7f2c186a087ce2051de3745b38ad1
Wasim Hanna <whanna@cardinalsolutions.com>
2018-03-09 11:24:15 -0500

    Basic IO Initial push

---
+++ BasicIO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileProcessingHelper;
+
+namespace BasicIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide an input file path");
+            var inputFilePath = Console.ReadLine();
+            Console.WriteLine("Please provide an output file path");
+            var outputFilePath = Console.ReadLine();
+            PreProcessFile.Start(inputFilePath, outputFilePath);
+        }
+    }
+}
